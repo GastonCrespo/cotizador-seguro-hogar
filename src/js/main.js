1a05e68c866a1cdb7868a15d8ed23b21d9023b33
Gaston Crespo <gastoncrespo02@gmail.com>
2026-03-31 12:26:56 -0300

    feat: improve insurance calculation logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,60 +1,73 @@
 import '../css/style.css'
+import { datosPropiedad, datosUbicacion } from './variables.js'
 
-
-const opcionesValidas = ['A','B','C','D','E','F','G']
+let fmPropiedad
+let fmUbicacion
 const costoBase = 1.16
-let factorMultiplicador = 0
-let tipoVivienda
+let tipoPropiedad
+let tipoUbicacion
 let metrosCuadrados
 
 metrosCuadrados=Number(prompt("ingrese metros cuadradaos"))
+
 do {
 
-    tipoVivienda = prompt(`Ingrese tipo de vivienda 
-A. Casa
-B. PH
-C. Dto edificio
-D. Barrio privado
-E. Oficina
-F. Local comercial
-G. Deposito logistica
-`).trim().toUpperCase()
-
-    if (!opcionesValidas.includes(tipoVivienda)) {
-        console.warn('Tipo de vivienda inválido, reintente')
+    tipoPropiedad = prompt(`Ingrese tipo de vivienda de su interes
+Casa
+P.H.
+Dto. edificio
+Barrio privado
+Oficina
+Local comercial
+Deposito logistica
+`).trim()
+let encontrado = false 
+    for(let propiedad of datosPropiedad) {
+        
+        if (propiedad.tipo.toUpperCase()==tipoPropiedad.toUpperCase()) {
+            fmPropiedad=propiedad // NO PONGO TIPO PROPEDAD PORQUE NECESITO GUARDAR TODO EL OBJETO
+            encontrado = true 
+            break
+        }  
     }
+     if (!encontrado) {
+            console.warn('Tipo de propiedad inválido, reintente')
+            
+        }
 
-} while (!opcionesValidas.includes(tipoVivienda))
+} while (!fmPropiedad)
 
-    switch (tipoVivienda) {
-        case 'A':
-            factorMultiplicador = 1.009
-            break;
-        case 'B':
-                factorMultiplicador = 1.005
-            break;
-        case 'C':
-                factorMultiplicador = 1.002
-            break;
-        case 'D':
-                factorMultiplicador = 1.019
-            break;
-        case 'E':
-                factorMultiplicador = 1.039
-            break;
-        case 'F':
-                factorMultiplicador = 1.041
-            break;
-        case 'G':
-                factorMultiplicador = 1.092
-            break;
-    }
+    
+do {
+    tipoUbicacion=prompt(`Ingrese ubicacion de su interes:
+        CABA
+        Tandil
+        Costa Atlantica
+        Patagonia Argentina
 
-    if(factorMultiplicador> 1.000 && Number.isInteger(metrosCuadrados))
+        `).trim().toUpperCase()
+        let encontrado = false
+        for(let ubicacion of datosUbicacion){
+            
+            if (ubicacion.tipo.toUpperCase()==tipoUbicacion) {
+                fmUbicacion=ubicacion
+                encontrado = true 
+                break
+            }
+        }
+         if(!encontrado)
+           {
+            console.warn("tipo de ubicacion no encontrada");
+           }
+
+} while (!fmUbicacion);
+    
+    //-----------------CALCULAMOS---------------
+if (fmPropiedad && fmUbicacion && Number.isInteger(metrosCuadrados))
 {
 alert('calculanding.... calculanding....')
 
-let precioPoliza = factorMultiplicador * metrosCuadrados * costoBase
+let precioPoliza = fmUbicacion.factor * metrosCuadrados * fmPropiedad.factor * costoBase
 
 console.log(`El monto total de la poliza es de $${precioPoliza.toFixed(4)}`)
 
